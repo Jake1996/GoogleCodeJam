2fbcc60e7a3d0471f71b8540f58625d18e707b9d
Jake1996 <jatinpm96@gmail.com>
2016-11-26 04:05:28 +0530

    NumberGame done

--- NumberGame.java
+++ NumberGame.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
-
+/**
+ * https://code.google.com/codejam/contest/544101/dashboard#s=p2&a=2
+ * @author jatin
+ */
 public class NumberGame {
-	final static int dp[][] = new int[1000001][1000001];
+	public static double phi = 1.61803398875;
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int tc = in.nextInt();
@@ -12,30 +15,24 @@ public class NumberGame {
 			A2 = in.nextInt();
 			B1 = in.nextInt();
 			B2 = in.nextInt();
-			int count=0;
-			for(int i=0;i<1000001;i++)
-				for(int j=0;j<=i;j++)
-					dp[i][j] = -1;
-			for(int i=A1;i<=A2;i++) 
-				for(int j=B1;j<=B2;j++) {
-					if(i>j) {
-						if(dp[i][j]==-1)
-							dp[i][j]=winner(i,j)==true?1:0;
-						count+=dp[i][j];
-					}
-					else {
-						if(dp[j][i]==-1)
-							dp[j][i]=winner(j,i)==true?1:0;
-						count+=dp[j][i];
-					}
-				}
+			long count=0;
+			for(int j=B1;j<=B2;j++)
+				count+=winner(A1,A2, j);
+			for(int j=A1;j<=A2;j++)
+				count+=winner(B1,B2, j);
+			
 			System.out.println("Case #"+(temp-tc)+": "+count);
 		}
 		in.close();
 	}
-	public static boolean winner(int a,int b) {
-		if(b==0) return true;
-		if(a>=2*b) return true;
-		return !winner(b,a-b);
+	public static long winner(int a1,int a2,int b) {
+		double x = b*phi;
+		if(a1>=Math.ceil(x)) {
+			return a2-a1+1;
+		}
+		if(a2>=Math.ceil(x)) {
+			return 1+a2-(int)Math.ceil(x);
+		}
+		return 0;
 	}
 }
