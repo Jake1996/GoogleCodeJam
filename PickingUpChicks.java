3440515a2ba00b3d6a9fd397ce65dcbc92a44c23
Jake1996 <jatinpm96@gmail.com>
2016-12-13 15:15:34 +0530

    picking up chicks

--- PickingUpChicks.java
+++ PickingUpChicks.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
-
+/**
+ * https://code.google.com/codejam/contest/635101/dashboard#s=p1
+ * @author jake
+ *
+ */
 public class PickingUpChicks {
-	public static final int dp[][]= new int[1001][51]; 
+	public static final int dp[]= new int[51]; 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int tc = in.nextInt();
@@ -18,22 +22,39 @@ public class PickingUpChicks {
 				x[i] = in.nextInt();
 			for(int i=0;i<N;i++)
 				v[i] = in.nextInt();
-			for(int i=0;i<T;i++)
-				for(int j=0;j<N;j++) {
-					dp[i][j] = x[j]+v[j]*i;
-				}
-			System.out.println("Case #"+(temp-tc)+": "+solve(N,K,B,T));
+			for(int j=0;j<N;j++) {
+				dp[j] = x[j]+v[j]*T;
+			}
+			int count = 0;
+			for(int i=0;i<N;i++) {
+				if(dp[i]>=B) count++;
+			}
+			if(count<K)
+				System.out.println("Case #"+(temp-tc)+": "+"IMPOSSIBLE");
+			else
+				System.out.println("Case #"+(temp-tc)+": "+solve(N,K,B));
 		}
-		System.out.println("done");
 		in.close();
 	}
-	public static int solve(int n,int k,int b,int t) {
-		for(int i=0;i<t;i++) {
-			for(int j=n-1;j>=0;j--) {
-				
+	public static int solve(int n,int k,int b) {
+		int count = 0;
+		int temp;
+		for(int i=n-1;i>=n-k;i--) {
+			if(dp[i]<b) {
+				int j=0;
+				for(j=i-1;j>=0;j--) {
+					if(dp[j]>=b) break;
+				}
+				temp=dp[j];
+				while(j!=i) {
+					dp[j]=dp[j+1];
+					j++;
+					count++;
+				}
+				dp[i]=temp;
 			}
 		}
-		return 0;
+		return count;
 	}
 	
 }
