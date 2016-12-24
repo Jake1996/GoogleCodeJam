312162a756792f22216ba54e5a588274e0674059
Jake1996 <jatinpm96@gmail.com>
2016-12-24 10:07:58 +0530

    prisoners and chess board

--- BribeThePrisoners.java
+++ BribeThePrisoners.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
 import java.util.Scanner;
-
+/**
+ * https://code.google.com/codejam/contest/189252/dashboard#s=p2
+ * @author jake
+ *
+ */
 public class BribeThePrisoners {
 	public static int arr[] = new int[101];
-	public static boolean prisoners[] = new boolean[10001];
+	public static int prisoners[][] = new int[10001][10001];
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int tc = in.nextInt();
@@ -14,43 +19,30 @@ public class BribeThePrisoners {
 			for(int i=0;i<Q;i++) {
 				arr[i] = in.nextInt();
 			}
-			System.out.println("Case #"+(temp-tc)+": "+ans(Q-1,P));
-			for(int i=0;i<=P;i++) prisoners[i] = false;
+			Arrays.sort(arr,0,Q-1);
+			for(int i=1;i<=P;i++)
+				for(int j=1;j<=P;j++)
+					prisoners[i][j] = -1;
+			System.out.println("Case #"+(temp-tc)+": "+ans(1,P,Q,P));
 		}
 		in.close();
 	}
-	public static int ans(int q,int p) {
-		int count = 0;
-		int i=0;
-		int j=q;
-		int low=1;
-		int high = p;
-		while(i<=j) {
-			if(arr[i]-low>high-arr[j]) {
-				count+=arr[i]-low;
-				count+=high-arr[i];
-				low=arr[i]+1;
-				i++;
+	public static int ans(int start,int end,int max,int P) {
+		if(start>P||end>P)
+			return 0;
+		if(prisoners[start][end]==-1) {
+			int min = Integer.MAX_VALUE;
+			for(int i=0;i<max;i++) {
+				if(arr[i]>end) break;
+				if(arr[i]>=start&&arr[i]<=end)
+					min = Math.min(min, end-start+ans(start,arr[i]-1 , max,P)+ans(arr[i]+1, end, max,P));
 			}
-			else {
-				count+=arr[j]-low;
-				count+=high-arr[j];
-				high=arr[j]-1;
-				j--;
-			}
+			if(min==Integer.MAX_VALUE)
+				prisoners[start][end]=0;
+			else
+				prisoners[start][end]=min;
 		}
-		return count;
-	}
-	public static int ans(int start,int end,int max) {
-		if(arr[(start+end)/2]==-1) return 0;
-		if(start<0||start>end) return 0;
-		int mid = arr[(start+end)/2];
-		arr[(start+end)/2] = -1;
-		prisoners[mid] = true;
-		int count = 0;
-		for(int i=mid-1;i>=1&&!prisoners[i];i--,count++) ;
-		for(int i=mid+1;i<=max&&!prisoners[i];i++,count++) ;
-		return count+ans(start, (start+end)/2-1, max)+ans((start+end)/2+1, end,max);
+		return prisoners[start][end];
 	}
 
 }
--- MakingChessBoards.java
+++ MakingChessBoards.java
@@ -1,15 +1,103 @@
 import java.util.Scanner;
-
+/**
+ * https://code.google.com/codejam/contest/619102/dashboard#s=p2
+ * @author jake
+ *
+ */
 public class MakingChessBoards {
+	public static int grid[][] = new int[512][512];
+	public static int dp[][] = new int[512][512];
+	public static int ans[] = new int[513];
+	public static boolean visited[][] = new boolean[512][512];
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int tc = in.nextInt();
 		int temp = tc;
+		int m,n;
+		int cnt;
+		String s;
 		while(tc--!=0) {
-			
-			System.out.println("Case #"+(temp-tc)+": ");
+			m = in.nextInt();
+			n = in.nextInt();
+			for(int i=0;i<m;i++) {
+				s = in.next();
+				int k;
+				for(int j=0;j<n/4;j++) {
+					k = Integer.parseInt(s.charAt(j)+"", 16);
+					for(int l=0;l<4;l++) {
+						int t = 1<<l;
+						if((t&k)!=0) 
+							grid[i][j*4+4-l-1]=0;
+						else
+							grid[i][j*4+4-l-1]=1;
+					}
+				}
+			}
+			for(int i=0;i<513;i++) ans[i] = 0;
+			calculate(m,n);
+			cnt=0;
+			for(int i=0;i<513;i++) if(ans[i]>0) cnt++;
+			System.out.println("Case #"+(temp-tc)+": "+cnt);
+			for(int i=512;i>0;i--) if(ans[i]>0) System.out.println(i+" "+ans[i]);
 		}
 		in.close();
 
+	}
+	public static void calculate(int m,int n) {
+		for(int i=0;i<m;i++) 
+			for(int j=0;j<n;j++)
+				dp[i][j] = 1;
+		int starti,startj,length;
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<n;j++) {
+				if(i!=0&&j!=0&&dp[i-1][j-1]>1) {
+					dp[i][j] = dp[i-1][j-1]-1;
+				}
+				boolean outer=true;
+				while(outer) {
+					starti=dp[i][j]+i;
+					startj=dp[i][j]+j;
+					if(starti>=m||startj>=n) break;
+					length = dp[i][j]+1;
+					boolean increment = true;
+					for(int k=0;k<length&&increment;k++) {
+						if(grid[i+k][startj]==grid[i+k][startj-1])
+							increment = false;
+						if(grid[starti][j+k]==grid[starti-1][j+k])
+							increment = false;
+					}
+					if(increment==true) {
+						dp[i][j]++;
+					}
+					else {
+						outer=false;
+					}
+				}
+			}
+		}
+		for(int i=0;i<m;i++) 
+			for(int j=0;j<n;j++)
+				visited[i][j] = false;
+		for(int z=Math.min(m, n);z>0;z--) {
+			for(int i=0;i<m;i++) {
+				for(int j=0;j<n;j++) {
+					if(!visited[i][j]&&dp[i][j]>=z&&check(i,j,z)) {
+						for(int p=i;p<z+i;p++)
+							for(int q=j;q<z+j;q++) {
+								visited[p][q]=true;
+							}
+						ans[z]++;
+					}
+				}
+			}
+		}	
+	}
+	public static boolean check(int i,int j,int len) {
+		for(int l=i;l<i+len;l++) {
+			for(int m=j;m<j+len;m++) {
+				if(visited[l][m]) return false;
+			}
+		}
+		return true;
 	}
 }
