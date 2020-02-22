0360fa6f018d5e0144d0b5f1e6fb6af929bdb397
Jatin Mohan <jpm001x@homeoffice.wal-mart.com>
2020-02-22 21:45:46 +0530

    Java path fixes

--- AlienLanguage.java
+++ AlienLanguage.java
@@ -1,6 +1,10 @@
+package GoogleCodeJam;
+
 import java.util.Scanner;
+
 /**
  * https://code.google.com/codejam/contest/90101/dashboard#s=p0
+ * 
  * @author jatin
  *
  */
@@ -8,49 +12,50 @@ public class AlienLanguage {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int l,d,n;
+		int l, d, n;
 		l = in.nextInt();
 		d = in.nextInt();
 		n = in.nextInt();
 		String lang[] = new String[d];
-		for(int i =0;i<d;i++)
+		for (int i = 0; i < d; i++)
 			lang[i] = in.next();
 		int tc = n;
 		int temp = tc;
-		while(tc--!=0) {
+		while (tc-- != 0) {
 			String word = in.next();
-			String choices[] = split(word,l);
+			String choices[] = split(word, l);
 			int count = 0;
-			for(int i = 0;i < d;i++){
+			for (int i = 0; i < d; i++) {
 				boolean flag = true;
-				for(int j = 0; j <l;j++) {
-					if(!choices[j].contains(Character.toString(lang[i].charAt(j)))){
+				for (int j = 0; j < l; j++) {
+					if (!choices[j].contains(Character.toString(lang[i].charAt(j)))) {
 						flag = false;
 						break;
 					}
 				}
-				if(flag) count++;
+				if (flag)
+					count++;
 			}
-			System.out.println("Case #"+(temp-tc)+": "+count);
+			System.out.println("Case #" + (temp - tc) + ": " + count);
 		}
 		in.close();
 	}
-	public static String[] split(String word,int l) {
+
+	public static String[] split(String word, int l) {
 		String set[] = new String[l];
 		boolean bracket = false;
 		int index = 0;
-		for(int i =0;i<l;i++) {
-			if(word.charAt(index)=='(')
+		for (int i = 0; i < l; i++) {
+			if (word.charAt(index) == '(')
 				bracket = true;
-			if(bracket) {
+			if (bracket) {
 				int startIndex = index;
-				while(word.charAt(index)!=')') {
+				while (word.charAt(index) != ')') {
 					index++;
 				}
 				bracket = false;
-				set[i] = word.substring(startIndex+1,index);
-			}
-			else
+				set[i] = word.substring(startIndex + 1, index);
+			} else
 				set[i] = Character.toString(word.charAt(index));
 			index++;
 		}
